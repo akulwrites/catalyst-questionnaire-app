c99d4487032620c599333cee1b3989f01b4aedfd
Akul <akulwritess@gmail.com>
2025-07-29 22:29:42 +0530

    dynamic env url for frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@ const { sequelize} = require('./models');
 
 sequelize.sync({ alter: true }).then(() => {
     app.listen(process.env.PORT || 3001, () => {
-        console.log('Server running on port 3001');
+        console.log(`Server running on port ${process.env.PORT || 3001}`);
     });
 });
--- frontend/endpoints/api.js
+++ frontend/endpoints/api.js
@@ -1,5 +1,7 @@
-export const QUESTIONS = "http://localhost:3001/api/questions";
+const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:3001";
 
-export const GET_ALL_SECTIONS = "http://localhost:3001/api/sections";
+export const QUESTIONS = `${apiUrl}/api/questions`;
 
-export const GET_QUESTION_BY_ID = (id) => `http://localhost:3001/api/questions/${id}`;
+export const GET_ALL_SECTIONS = `${apiUrl}/api/sections`;
+
+export const GET_QUESTION_BY_ID = (id) => `${apiUrl}/api/questions/${id}`;
