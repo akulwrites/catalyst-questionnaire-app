20e327012399cbf9e604d0fa7449f393afd4a090
Akul <akulwritess@gmail.com>
2025-07-29 22:10:35 +0530

    added the prod db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,18 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('mydb', 'myuser', 'mypassword', {
-    host: 'localhost',
+// const sequelize = new Sequelize('mydb', 'myuser', 'mypassword', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-});
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});  
 
 module.exports = sequelize;
