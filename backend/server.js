9f3e623b51295bea21edbeb45be02d4a4bbc0b85
Akul <akulwritess@gmail.com>
2025-07-29 21:58:23 +0530

    added the process env port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const { sequelize} = require('./models');
 
 sequelize.sync({ alter: true }).then(() => {
-    app.listen(3001, () => {
+    app.listen(process.env.PORT || 3001, () => {
         console.log('Server running on port 3001');
     });
 });
