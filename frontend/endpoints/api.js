7df1b3e682a87164a303c8ecf6bb68e24628c03f
Akul <akulwritess@gmail.com>
2025-07-29 22:46:53 +0530

    env varn name change

--- frontend/endpoints/api.js
+++ frontend/endpoints/api.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.NEXT_APP_BACKEND_URL || "http://localhost:3001";
+const apiUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
 console.log(`API URL: ${apiUrl}`);
 
 export const QUESTIONS = `${apiUrl}/api/questions`;
